f3f50a99eeb898e4a9ed3b6f06b8039eda6ee653
Darius Oprea <63239571+opreaDO@users.noreply.github.com>
2023-01-02 17:22:43 +0000

    Add files via upload

--- files/GERT/include/flywheel.h
+++ files/GERT/include/flywheel.h
@@ -2,9 +2,11 @@
 #include "main.h"
 
 extern bool flySpinning;
-extern int fwSpeeds[4];
-extern int fwIndex;
+
 extern sylib::SpeedControllerInfo flyController;
 extern sylib::Motor fly;
-extern void setFW (int power);
 extern void flywheel (void);
+
+extern int fwIndex;
+extern int fwSpeed[3];
+
