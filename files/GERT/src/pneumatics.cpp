d32b759bd0da6c3e114a9a15b4223354babf86b0
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2023-01-16 16:32:03 +0000

    Update pneumatics.cpp

--- files/GERT/src/pneumatics.cpp
+++ files/GERT/src/pneumatics.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "pneumatics.h"
 
+bool lipToggle = false;
 ADIDigitalOut indexer({{13, 8}});
 ADIDigitalOut lip({{13, 7}});
 ADIDigitalOut stringS({{13, 6}});
@@ -8,7 +9,16 @@ ADIDigitalOut stringS({{13, 6}});
 void pneumatics (void) {
     while (true) {
         if (master.get_digital(DIGITAL_R2)) {indexer.set_value(true); delay(200);} else {indexer.set_value(false);}
-        if (master.get_digital(DIGITAL_Y)) {lip.set_value(true); delay(200);} else {lip.set_value(false);}
+        if (master.get_digital(DIGITAL_Y)) {
+            if (lipToggle) {
+                lip.set_value(false);
+            }
+            else {
+                lip.set_value(true);
+            }
+            lipToggle = !lipToggle;
+        }
+        
         if (master.get_digital(DIGITAL_X)) {stringS.set_value(true); delay(200); stringS.set_value(false);}
         delay(10);
     }}
