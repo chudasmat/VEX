49d94fce706179f8f430bced939ab163476eac83
Trishan Chudasma <trishan759@gmail.com>
2023-02-19 18:39:42 +0000

    .

--- files/GERT/src/main.cpp
+++ files/GERT/src/main.cpp
@@ -5,10 +5,7 @@
 #include "pneumatics.h"
 #include "pros/rtos.hpp"
 
-// L2 = RED
-// R2 = BLUE
-
-int totalTime = -10;
+int startTime;
 int desiredVal = 3000;
 
 void disabled() {}
@@ -16,30 +13,27 @@ void competition_initialize() {}
 
 void initialize() {
 	sylib::initialize();
-	//okapiController.setText(0, 0, "SELECT COLOUR");
 	static Gif gif("/usd/slideshow.gif", lv_scr_act());
 	rgb();
 }
 
 void autonomous() {
-	//okapiController.clearLine(0);
-	chassisDrive->setState({0_in, 0_in, 0_deg});
 	rollerAuton();
 }
 
 void opcontrol() {
+	startTime = millis();
 	lip.set_value(true);
 	lipBool = true;
 	fwPower = 9000;
 	
 	while (true) {
 		std::uint32_t now = millis();
-		totalTime += 10;
 		chassisControl();
 		flywheel();
 		intakeControl();
 		pneumatics();
-		if (totalTime >= 94000) {endgame = true;}
-//		printf("%d,%d,%f\n", millis(), desiredVal,fly.get_velocity();
+		if ((now - startTime) > 95000) {endgame = true;}
+//		printf("%d,%d,%f\n", smillis(), desiredVal,fly.get_velocity();
 		Task::delay_until(&now, 10);
 	}}
