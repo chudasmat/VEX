b846c053079d9527e8046ca842055e395f03ad55
Trishan Chudasma <trishan759@gmail.com>
2024-01-02 17:22:12 +0000

    .

--- files/OverUnder/ouMain/include/globals.h
+++ files/OverUnder/ouMain/include/globals.h
@@ -9,18 +9,26 @@ extern void chassisControl(void);
 
 // kicker
 extern void kickerControl(void);
+extern void kickMacro(void);
+extern Motor kicker;
 
 // intake
 extern void intakeControl(void);
+extern Motor intake;
 
 // lift
 extern void liftControl(void);
+extern ADIDigitalOut lift;
 
 // wings
 extern void wingsControl(void);
+extern ADIDigitalOut bigWings;
+extern ADIDigitalOut miniWing;
 
 // climber
 extern void climberControl(void);
+extern Motor roll;
+extern ADIDigitalOut wheelExtend;
 
 // rgb
 extern void rgb(void);
--- files/OverUnder/ouMain/src/kicker.cpp
+++ files/OverUnder/ouMain/src/kicker.cpp
@@ -1,11 +1,16 @@
 #include "main.h"
 bool kickerOn = false;
 Motor kicker(7, false);
+ADIDigitalIn kickLimit(5);
 
 void kickerControl(void) {
     if (master.get_digital_new_press(DIGITAL_B)) {
         if (kickerOn) {kicker.brake();}
         else {kicker.move(127);}
         kickerOn = !kickerOn;
+    }
+    if (master.get_digital_new_press(DIGITAL_A)) {
+        if (kickLimit.get_value() == 0) {kicker.move(90);}
+        else if (kickLimit.get_value() == 1) {kicker.brake();}
     }
 }
