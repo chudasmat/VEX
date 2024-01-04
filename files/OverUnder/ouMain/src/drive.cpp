f52644bf00f1a05916830198bb2c1f08e4610e02
Trishan Chudasma <trishan759@gmail.com>
2024-01-04 18:35:05 +0000

    .

--- files/OverUnder/ouMain/src/drive.cpp
+++ files/OverUnder/ouMain/src/drive.cpp
@@ -3,9 +3,9 @@
 float leftInput; float rightInput;
 float leftPower; float rightPower;
 
-Motor rightA(1, E_MOTOR_GEARSET_06, true);
-Motor rightB(2, E_MOTOR_GEARSET_06, true);
-Motor rightC(3, E_MOTOR_GEARSET_06, true);
+Motor rightA(2, E_MOTOR_GEARSET_06, true);
+Motor rightB(3, E_MOTOR_GEARSET_06, true);
+Motor rightC(4, E_MOTOR_GEARSET_06, true);
 Motor leftA(8, E_MOTOR_GEARSET_06, false);
 Motor leftB(9, E_MOTOR_GEARSET_06, false);
 Motor leftC(10, E_MOTOR_GEARSET_06, false);
@@ -14,14 +14,14 @@ MotorGroup rightDrive({rightA, rightB, rightC});
 Imu imu(16);
 
 void chassisControl (void) {
-    leftInput = master.get_analog(ANALOG_LEFT_Y);
-    rightInput = master.get_analog(ANALOG_RIGHT_Y);
+    leftInput = master.get_analog(ANALOG_LEFT_Y) / 127.0;
+    rightInput = master.get_analog(ANALOG_RIGHT_Y) / 127.0;
 	leftPower = (atan((2 * leftInput - (leftInput / std::abs(leftInput))) * 5.5) / (2 * atan(5.5))) + (leftInput / (2 * std::abs(leftInput))) * 127;
 	rightPower = (atan((2 * rightInput - (rightInput / std::abs(rightInput))) * 5.5) / (2 * atan(5.5))) + (rightInput / (2 * std::abs(rightInput))) * 127;
 	
-	leftA.move(leftPower);
-	leftB.move(leftPower);
-	leftC.move(leftPower);
-	rightA.move(rightPower);
-	rightB.move(rightPower);
-	rightC.move(rightPower);}
+	leftA.move(leftPower * 127);
+	leftB.move(leftPower * 127);
+	leftC.move(leftPower * 127);
+	rightA.move(rightPower * 127);
+	rightB.move(rightPower * 127);
+	rightC.move(rightPower * 127);}
