b4c929531dcf3e723ed339be72aa82a46ef12975
Trishan Chudasma <trishan759@gmail.com>
2024-01-11 19:58:01 +0000

    EZ Beta

--- files/OverUnder/beta/src/drive.cpp
+++ files/OverUnder/beta/src/drive.cpp
@@ -3,12 +3,12 @@
 float leftInput; float rightInput;
 float leftPower; float rightPower;
 
-Motor rightA(5, E_MOTOR_GEARSET_06, false);
-Motor rightB(6, E_MOTOR_GEARSET_06, false);
-Motor rightC(10, E_MOTOR_GEARSET_06, false);
 Motor leftA(2, E_MOTOR_GEARSET_06, true);
 Motor leftB(3, E_MOTOR_GEARSET_06, true);
 Motor leftC(4, E_MOTOR_GEARSET_06, true);
+Motor rightA(5, E_MOTOR_GEARSET_06, false);
+Motor rightB(6, E_MOTOR_GEARSET_06, false);
+Motor rightC(10, E_MOTOR_GEARSET_06, false);
 MotorGroup leftDrive({leftA, leftB, leftC});
 MotorGroup rightDrive({rightA, rightB, rightC});
 Imu imu(11);
