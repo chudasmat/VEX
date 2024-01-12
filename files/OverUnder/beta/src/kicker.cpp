830cea8b43fc97be0eb2800269bab8b0bab55d95
Trishan Chudasma <trishan759@gmail.com>
2024-01-12 20:02:58 +0000

    EZ Beta

--- files/OverUnder/beta/src/kicker.cpp
+++ files/OverUnder/beta/src/kicker.cpp
@@ -1,13 +1,14 @@
 #include "main.h"
+#include "pros/motors.h"
 bool kickerOn = false;
-Motor kicker(7, true);
+Motor kicker(7, E_MOTOR_GEAR_200, true);
 ADIDigitalIn kickLimit(5);
 Optical optical(18);
 
 void kickerControl(void) {
     if (master.get_digital_new_press(DIGITAL_B)) {
         if (kickerOn) {kicker.brake();}
-        else {kicker.move(127);}
+        else {kicker.move_velocity(200);}
         kickerOn = !kickerOn;
     }
     if (master.get_digital(DIGITAL_A)) {
