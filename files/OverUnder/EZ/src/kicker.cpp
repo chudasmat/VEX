a041f5f59456b2250ca8f45f344275bdd51d8bdb
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2024-01-16 09:02:18 +0000

    .

--- files/OverUnder/EZ/src/kicker.cpp
+++ files/OverUnder/EZ/src/kicker.cpp
@@ -6,7 +6,7 @@ ADIDigitalIn kickLimit(5);
 Optical optical(2);
 
 void kickerControl(void) {
-    if (master.get_digital_new_press(DIGITAL_DOWN)) {
+    if (master.get_digital_new_press(DIGITAL_X)) {
         if (kickerOn) {kicker.brake();}
         else {kicker.move_velocity(120);}
         kickerOn = !kickerOn;
