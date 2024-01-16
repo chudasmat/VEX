fd9d3b1dd3b71b1dd16536b05e9970ecb35011e7
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2024-01-16 09:04:21 +0000

    .

--- files/OverUnder/EZ/src/wings.cpp
+++ files/OverUnder/EZ/src/wings.cpp
@@ -3,10 +3,10 @@
 #include "pros/adi.hpp"
 #include "pros/misc.h"
 
-bool bigOn = false;
-bool miniOn = false;
+bool bigOn = false; bool miniOn = false; bool beachOn = false;
 ADIDigitalOut bigWings(2);
 ADIDigitalOut miniWing(3);
+ADIDigitalOut beach(4);
 
 void wingsControl(void) {
     if (master.get_digital_new_press(DIGITAL_Y)) {
@@ -16,5 +16,9 @@ void wingsControl(void) {
     if (master.get_digital_new_press(DIGITAL_RIGHT)) {
         miniWing.set_value(!miniOn);
         miniOn = !miniOn;
+    }
+    if (master.get.digital_new_press(DIGITAL_DOWN)) {
+        beach.set_value(!beachOn);
+        beachOn = !beachOn;
     }
 }
