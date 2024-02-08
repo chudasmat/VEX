f7d7efe84c7f7e030a256b5d03bcdc72d476ece7
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2024-02-08 10:02:59 +0000

    .

--- files/OverUnder/EZ/src/main.cpp
+++ files/OverUnder/EZ/src/main.cpp
@@ -4,6 +4,8 @@ Controller master(CONTROLLER_MASTER);
 
 void initialize() {
 	chassis.initialize();
+	default_constants();
+	static Gif gif("/usd/slideshow.gif", lv_scr_act());
 	chassis.set_curve_default(3.5, 3.5);
 }
 
@@ -22,25 +24,17 @@ void autonomous() {
 
 void opcontrol() {
 	sylib::initialize();
-	static Gif gif("/usd/slideshow.gif", lv_scr_act());
 	rgb();
 	
 	liftLock.set_value(1);
 	kicker.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 	int count = 0;
 	chassis.set_drive_brake(E_MOTOR_BRAKE_COAST);
-	while (true) {
-
-		if (!(count % 25)) {
-      		master.print(0, 0, "Optical: %d", optical.get_proximity());
-    	}
-    	count++;
-
-		chassisControl();
-		kickerControl();
-		intakeControl();
-		liftControl();
-		wingsControl();
-		pros::delay(10);
+	chassisControl();
+	kickerControl();
+	intakeControl();
+	liftControl();
+	wingsControl();
+	pros::delay(10);
 	}
 }
