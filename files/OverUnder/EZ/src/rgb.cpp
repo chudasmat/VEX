13cf18e452a107b52eff183014c0694ab1a2e594
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2024-02-08 10:05:24 +0000

    .

--- files/OverUnder/EZ/src/rgb.cpp
+++ files/OverUnder/EZ/src/rgb.cpp
@@ -4,7 +4,7 @@ sylib::Addrled ledStrip1(22, 5, 20);
 sylib::Addrled ledStrip2(22, 6, 20);
 
 void rgb (void) {
-    ledStrip1.gradient(0xFF0000, 0xFF0005, 0, 0, true, true);
+	ledStrip1.gradient(0xFF0000, 0xFF0005, 0, 0, true, true);
 	ledStrip1.cycle(*ledStrip1, 4, true);
 	ledStrip2.gradient(0xFF0000, 0xFF0005, 0, 0, true, true);
 	ledStrip2.cycle(*ledStrip2, 4, true);}
