6d8aa0bdb036d49cb9cb0d62cd2efaa952235df1
Trishan Chudasma <81425404+chudasmat@users.noreply.github.com>
2024-04-18 11:24:30 +0100

    .

--- files/OverUnder/ezTEST/src/main.cpp
+++ files/OverUnder/ezTEST/src/main.cpp
@@ -6,6 +6,7 @@ void initialize() {
 	sylib::initialize();
 	static Gif gif("/usd/slideshow.gif", lv_scr_act());
 	rgb();
+	chassis.set_curve_default(3.5, 3.5);
 }
 
 void disabled() {}
@@ -15,7 +16,7 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-	master.print(0, 0, "PTO DRIVE");
+	master.print(0, 0, "COAST - PTO DRIVE");
 	while (true) {
 		chassisControl();
 		intakeControl();
